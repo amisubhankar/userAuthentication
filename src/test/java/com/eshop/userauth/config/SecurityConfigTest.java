576d273880c040f7ae5403ed7974f185f3864b2e
amisubhankar <subhankar.manna99@gmail.com>
2024-05-27 22:01:47 +0530

    Added Postman oidc client

---
+++ src/test/java/com/eshop/userauth/config/SecurityConfigTest.java
@@ -0,0 +1,43 @@
+package com.eshop.userauth.config;
+
+
+import com.eshop.userauth.repositories.JpaRegisteredClientRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SecurityConfigTest {
+
+    @Autowired
+    JpaRegisteredClientRepository clientRepository;
+
+    @Test
+    public void createAClient(){
+        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("oidc-client")
+                .clientSecret("$2a$12$KHQEnI6xvCi2bd.UEIuZVOjGILbxoiW2V1Hojnn1MsQT3kvnJzd3u")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri("https://oauth.pstmn.io/v1/callback")
+                .postLogoutRedirectUri("https://oauth.pstmn.io/v1/callback")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .scope("ADMIN")
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build();
+
+        clientRepository.save(oidcClient);
+    }
+}
